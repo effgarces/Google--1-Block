d722e16ae921f3a525c26c047731b4ebff620886
Emanuel Garcês <emanuel.garces@live.madeira-edu.pt>
2012-03-20 11:52:46 +0000

    Added Slovenian translation. Contributed by: Gregor Anželj

---
+++ plusone/lang/pt.utf8/blocktype.plusone.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-plusone
+ * @author     Emanuel Garcês
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Gregor Anzelj, gregor.anzelj@gmail.com
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Google +1';
+$string['description'] = 'Adiciona um botão +1 do Google';
+
+$string['showtitle'] = 'Mostrar título do bloco?';
+
+$string['size'] = 'Tamanho';
+
+$string['sizeesmall'] = 'Pequeno (15px)';
+$string['sizemedium'] = 'Médio (20px)';
+$string['sizestandard'] = 'Padrão (24px)';
+$string['sizetall'] = 'Vertical (60px)';
+
+$string['count'] = 'Contagem';
+
+$string['align'] = 'Alinhar';
+$string['alignleft'] = 'Esquerda';
+$string['aligncenter'] = 'Centro';
+$string['alignright'] = 'Direita';
+
+$string['previewonly'] = 'Apenas uma pré-visualização estática do botão +1';
+
+$string['sizedesc'] = 'Se seleccionar "Vertical" então o botão irá incluir a contagem de +1s e a definição "Contagem" será ignorada';
+$string['countdesc'] = 'Incluí uma contagem de +1s.';
+?>
---
+++ plusone/lang/sl.utf8/blocktype.plusone.php
@@ -0,0 +1,55 @@
+﻿<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-plusone
+ * @author     Emanuel Garcês
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Emanuel Garcês
+ *
+ * @translator  Gregor Anželj, gregor.anzelj@gmail.com
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Google +1';
+$string['description'] = 'Dodaj Google +1 gumb';
+
+$string['showtitle'] = 'Prikažem naslov bloka?';
+
+$string['size'] = 'Velikost';
+
+$string['sizeesmall'] = 'Majhno (15px)';
+$string['sizemedium'] = 'Srednje (20px)';
+$string['sizestandard'] = 'Običajno (24px)';
+$string['sizetall'] = 'Visoko (60px)';
+
+$string['count'] = 'Števec';
+
+$string['align'] = 'Poravnava';
+$string['alignleft'] = 'Levo';
+$string['aligncenter'] = 'Sredinsko';
+$string['alignright'] = 'Desno';
+
+$string['previewonly'] = 'Samo statični predogled +1 gumba';
+
+$string['sizedesc'] = 'Če je izbrana možnost "Visoko", potm bo gumb vseboval celotno število +1, nastavitev "Števec" pa ne bo upoštevana.';
+$string['countdesc'] = 'Vklučuje celotno število +1.';
+?>
